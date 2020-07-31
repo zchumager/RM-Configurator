c1980e3f045f4a3af2a96535b3542509495a80ea
Pedro Machado <pedro.machado@tala.co>
2020-07-30 23:40:15 -0600

    readme update

---
+++ README.md
@@ -0,0 +1,29 @@
+**BROADLINK RM CONFIGURATOR**
+
+Python CLI Tool for retrieving network information of any Broadlink device like RM2, RM3 and RM4 IR/RF Hub  
+. At present, the following devices are currently supported:  
+  
+
+ - RM Pro (referred to as RM2 in the codebase)
+ - A1 sensor platform devices are supported  
+ - RM3 mini IR blaster  
+ - RM4 and RM4C mini blasters  
+ - There is currently no support for the cloud API.
+
+  
+  
+**Put device into AP Mode**  
+Setup a new device on your local wireless network:  
+  
+
+ 1. Long press the reset button until the blue LED is blinking quickly (like 7 seconds).  
+ 2. Long press again until blue LED is blinking slowly (like 14 seconds).  
+ 3. Manually connect to the WiFi SSID named BroadlinkProv.
+ 4. Run rm_config with python3 interpreter
+ 
+> python3 rm_config.py -s SSID_2.4 -p SSID_PASSWORD -m  MODE
+
+Mode (0 = none, 1 = WEP, 2 = WPA1, 3 = WPA2, 4 = WPA1/2)
+
+Once the RM Config is already config just run the CLI with no args
+> python3 rm_config.py
---
+++ app/rm_config.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+import broadlink
+import argparse
+from errno import ENETUNREACH
+
+
+def setup_rm(ssid, password, sec_mode):
+    """
+    :param ssid: WiFi SSID
+    :param password: WiFi Password
+    :param sec_mode: WiFi Security Mode
+    :return:
+    """
+    try:
+        broadlink.setup(ssid, password, sec_mode)
+    except IOError as e:
+        if e.errno == ENETUNREACH:
+            print("sending packages")
+
+
+def get_rm_info(devices):
+    """
+    :param devices: A List of broadlink objects
+    :return: a dict with the first broadlink object found
+    """
+    print("RM INFO")
+
+    model = devices[0].model
+    ip_address = devices[0].host[0]
+    bin_mac = devices[0].mac
+    mac_address = ':'.join(format(e, '02x').upper() for e in bin_mac)
+    raw_mac = mac_address.replace(":", "").lower()
+
+    return {
+        "model": model,
+        "ip_address": ip_address,
+        "binary_mac": bin_mac,
+        "mac_address": mac_address,
+        "raw_mac": raw_mac
+    }
+
+
+def discover_devices():
+    devices = broadlink.discover(timeout=5)
+    if len(devices) > 0:
+        rm_info = get_rm_info(devices)
+        print(f"Device Information: {rm_info}")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='RM Mini Configurator Python CLI Tool'
+    )
+
+    parser.add_argument('-s', help="WiFi SSID")
+    parser.add_argument('-p', help="WiFi Password")
+    parser.add_argument('-m', help="WiFi Security Mode")
+
+    args = parser.parse_args()
+
+    if args.s and args.p and args.m:
+        setup_rm(args.s, args.p, int(args.m))
+        devices = broadlink.discover(timeout=5)
+        if not devices:
+            input("WAIT!!! PRESS ENTER JUST WHEN YOUR COMPUTER CONNECTS TO YOUR HOME NETWORK AGAIN...")
+
+        discover_devices()
+    else:
+        discover_devices()
+
+
+if __name__ == '__main__':
+    main()
