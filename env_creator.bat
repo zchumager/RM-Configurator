5c6434099519497f27fd624b73ccb8dfed425f00
zchumager <arnoldo.machado@hotmail.com>
2020-12-17 10:00:21 -0700

    adding comments on env_creator.bat

--- env_creator.bat
+++ env_creator.bat
@@ -1,3 +1,8 @@
+rem  install pipenv
 python3 -m pip install pipenv
+
+rem create a new environment
 python3 -m pipenv shell
+
+rem install the dependencies into the new environment
 python3 -m pip install -r requirements.txt
