d0571a7bc989e8b11b4462bd733fb94c1a8d1cfc
zchumager <arnoldo.machado@hotmail.com>
2020-12-16 18:40:53 -0700

    adding comments on env_creator.sh

--- env_creator.sh
+++ env_creator.sh
@@ -1,3 +1,8 @@
+# install pipenv
 python3 -m pip install pipenv
+
+# create a new environment
 python3 -m pipenv shell
+
+# install the dependencies into the new environment
 python3 -m pip install -r requirements.txt
